9ebc8ec7f7d75bb1c274742725438085d89add30
areum514 <21500514@handong.edu>
2020-05-29 17:30:17 +0900

    1.0 members models finished

---
+++ members/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+@admin.register(models.member)
+class MemberAdim(admin.ModelAdmin):
+    """Item Admin Definition"""
+    list_display = ("name", )
+
+    def __str__(self):
+        return self.name
---
+++ members/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class member(models.Model):
+    """member Model Definition"""
+
+    name = models.CharField(max_length=140)
+    avatar = models.ImageField(upload_to="avatars", blank=True)
+    description = models.TextField()
