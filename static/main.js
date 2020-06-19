fbbd09de9c3b576f23a5ebfbfbba8bedf6e59b44
areum514 <21500514@handong.edu>
2020-06-19 22:21:52 +0900

    메뉴바 리팩토리

---
+++ static/main.js
@@ -0,0 +1,8 @@
+<script>
+    const toggleBtn = document.querySelector(".navbar-togleBtn");
+    const menu = document.querySelector(".navbar-menu");
+        toggleBtn.addEventListener("click", () => {
+        menu.classList.toggle('active')
+    });
+
+    </script>
